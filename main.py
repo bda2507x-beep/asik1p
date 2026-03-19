e850f6f5063044b8335a82d92b99e13a4404ebbb
Khanzat Akhmetov <252772@astanait.edu.kz>
2026-03-19 15:15:22 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#task 1 (variant A)
+Customer_name = input( "Enter your name: " )
+Product_name = input( "Enter product name : " )
+Price_penu_KZ= float(input( "Enter price per unit (KZT): " ))
+Quantity = int(input("Enter quantity: "))
+
+# task 2
+subtotal = Price_penu_KZ * Quantity
+
+
+if subtotal > 5000:
+    f_total = subtotal * 0.90
+    discount = subtotal  * 0.10
+else:
+    print( subtotal)
+
+print ("=" *30 )
+print( "SHOP RECEIPT")
+print ( "=" * 30  )
+print ( "customer : " + Customer_name )
+print ( f"product : " + Product_name )
+print ( f"quantity : " + str(Quantity) )
+print( "-" *30)
+print( "subtotal : " + str(subtotal) )
+print( "discount : " + str(discount) )
+print("total = " + str(f_total) )
+print ("=" * 30 )
+
+print("")
+print("Discount applied:", subtotal > 5000)
+print("Paid more than 3000:", f_total > 3000)
+
+
+
